9191d63fa665997445acae461fbac55fbff76541
Kristen Peck <kpeck71@gmail.com>
2017-12-07 12:52:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -22,7 +22,7 @@ class App < Sinatra::Base
     @num1 = params[:num1].to_i
     @num2 = params[:num2].to_i
     @product = @num1 * @num2
-    "The answer is #{@product}"
+    "The answer is #{@product}".
   end
 
 end
